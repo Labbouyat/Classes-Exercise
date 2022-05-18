4d910edda193403bf0a7f48bc7ecbd527afe4050
Labbouyat <labbouyat@gmail.com>
2022-05-18 12:28:03 -0400

    completed

---
+++ Classes Exercise/Car.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Classes_Exercise
+{
+    public class Car
+    {
+        public Car(string makeInput, string modelInput, int yearInput)
+        {
+            this.Make = makeInput;
+            this.Model = modelInput;
+            this.Year = yearInput;
+        }
+
+
+        public Car()
+        {
+
+        }
+
+         public string Make { get; set; }
+         public string Model { get; set; }
+         public int Year { get; set; }
+
+
+        public void Honk ()
+        {
+            Console.WriteLine("Beep beep");
+        } 
+    }
+}
+    
---
+++ Classes Exercise/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Classes_Exercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Car ford = new Car();
+            ford.Make = "Ford";
+            ford.Model = "Explorer";
+            ford.Year = 2011;
+
+            var hyundai = new Car("Hyundai", "Sonata", 2011);
+
+            var chrysler = new Car()
+            {
+                Make = "Chrysler",
+                Model = "200 LX FWD",
+                Year = 2016
+            };
+
+            var carList = new List<Car>() { ford, chrysler, hyundai };
+
+            foreach (var car in carList)
+            {
+                Console.WriteLine($"Make: {car.Make}");
+                Console.WriteLine($"Model: {car.Model}");
+                Console.WriteLine($"Year: {car.Year}");
+            }
+        }
+    }
+}
